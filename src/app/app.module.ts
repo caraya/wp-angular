6895f36253016ef5d79c575f793c526414762449
Carlos <carlos.araya@gmal.com>
2018-04-30 01:06:08 -0700

    finished first pass of posts-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PostService } from './post.service';
 import { PostsListComponent } from './posts-list/posts-list.component';
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
-  declarations: [AppComponent, PostsListComponent],
+  declarations: [AppComponent, PostsListComponent, SafePipe],
   imports: [BrowserModule, HttpClientModule],
   providers: [PostService],
   bootstrap: [AppComponent]
